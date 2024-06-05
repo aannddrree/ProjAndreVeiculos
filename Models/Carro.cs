f1fa6a1e20818cdf337ad9aec55bcdb632811dff
aannddrree <aannddrree.silva@gmail.com>
2024-06-05 15:21:09 -0300

    v11

--- Models/Carro.cs
+++ Models/Carro.cs
@@ -3,6 +3,11 @@
     public class Carro
     {
         public string Placa { get; set; }
+        public string   Nome { get; set; }
+        public int AnoModelo { get; set; }
+        public int AnoFabricacao { get; set; }
+        public string Cor { get; set; }
+        public bool Vendido { get; set; }
 
     }
 }
