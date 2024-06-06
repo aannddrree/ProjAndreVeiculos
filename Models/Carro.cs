08ea2ce7cd0d2b2a7b3e56ab7f5d34ece8d26baf
aannddrree <aannddrree.silva@gmail.com>
2024-06-06 16:32:51 -0300

    v3

--- Models/Carro.cs
+++ Models/Carro.cs
@@ -1,8 +1,12 @@
-﻿namespace Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
 {
     public class Carro
     {
+        [Key]
         public string Placa { get; set; }
+
         public string   Nome { get; set; }
         public int AnoModelo { get; set; }
         public int AnoFabricacao { get; set; }
---
+++ ProjAPICarro/Data/ProjAPICarroContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace ProjAPICarro.Data
+{
+    public class ProjAPICarroContext : DbContext
+    {
+        public ProjAPICarroContext (DbContextOptions<ProjAPICarroContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Carro> Carro { get; set; } = default!;
+        public DbSet<Models.Pessoa>? Pessoas { get; set; } = default!;
+        public DbSet<Models.Cliente>? Cliente { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configura a chave primária na entidade raiz Pessoa
+            modelBuilder.Entity<Pessoa>()
+                .HasKey(p => p.Documento);
+        }
+
+
+
+    }
+}
